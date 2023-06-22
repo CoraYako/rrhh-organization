4a83ccd97e227e4bf4fcc01b96744592d050c422
Hector Cortez <updown.input@gmail.com>
2023-06-21 21:09:00 -0300

    initial commit

---
+++ department-service/src/test/java/org/rrhh/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rrhh.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ employee-service/src/test/java/org/rrhh/employee/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rrhh.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
