953eac6e263fea05b231a151de3dd8d5c38ed134
Hector Cortez <updown.input@gmail.com>
2023-07-23 15:06:05 -0300

    Fix: constants created
    Signed-off-by: Hector Cortez <updown.input@gmail.com>

---
+++ employee-service/src/main/java/org/rrhh/employee/domain/EmployeeConstants.java
@@ -0,0 +1,7 @@
+package org.rrhh.employee.domain;
+
+public class EmployeeConstants {
+
+    public static final String BASE_URI = "/api/v1/employees";
+    public static final String COLLECTION_NAME = "employees";
+}
