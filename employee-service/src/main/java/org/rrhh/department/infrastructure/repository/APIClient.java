022018d9122909942404d1c1e2b890ddb4e72317
Hector Cortez <updown.input@gmail.com>
2023-08-06 11:02:28 -0300

    Fix: renamed from DepartmentFeign
    Signed-off-by: Hector Cortez <updown.input@gmail.com>

--- employee-service/src/main/java/org/rrhh/department/infrastructure/repository/APIClient.java
+++ employee-service/src/main/java/org/rrhh/department/infrastructure/repository/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(url = DepartmentConstants.BASE_URL, name = DepartmentConstants.MICROSERVICE_NAME)
-public interface DepartmentFeign {
+public interface APIClient {
 
     @GetMapping(value = DepartmentConstants.GET_BY_CODE_URI)
     DepartmentResponseDTO getDepartmentByCode(@PathVariable String departmentCode);
