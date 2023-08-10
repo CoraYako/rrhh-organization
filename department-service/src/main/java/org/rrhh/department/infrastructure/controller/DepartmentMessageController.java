48238a9b2db6f965e05cebdc789696ce0f2c19e3
Hector Cortez <updown.input@gmail.com>
2023-08-09 23:23:39 -0300

    New: controller to test bus refresh endpoint when spring.cloud.message property is changed
    Signed-off-by: Hector Cortez <updown.input@gmail.com>

---
+++ department-service/src/main/java/org/rrhh/department/infrastructure/controller/DepartmentMessageController.java
@@ -0,0 +1,19 @@
+package org.rrhh.department.infrastructure.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentMessageController {
+
+    @Value("{spring.boot.message")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+}
