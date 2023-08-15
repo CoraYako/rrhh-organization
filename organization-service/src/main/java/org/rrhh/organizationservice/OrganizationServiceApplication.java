67a2d76d34dce39bdbe1e18e928374e40ab7a088
Hector Cortez <updown.input@gmail.com>
2023-08-15 16:00:15 -0300

    New: initial commit for organization-service project
    Signed-off-by: Hector Cortez <updown.input@gmail.com>

---
+++ organization-service/src/main/java/org/rrhh/organizationservice/OrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rrhh.organizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServiceApplication.class, args);
+	}
+
+}
