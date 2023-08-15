7c6da1ffee50b53f1796b535c11f2b76694496d0
Hector Cortez <updown.input@gmail.com>
2023-08-15 14:32:25 -0300

    Feat: circuit breaker implementing resilience4j with a fallback method
    Signed-off-by: Hector Cortez <updown.input@gmail.com>

--- employee-service/src/main/java/org/rrhh/department/application/implementation/DepartmentFindByCode.java
+++ employee-service/src/main/java/org/rrhh/department/application/implementation/DepartmentFindByCode.java
@@ -1,5 +1,6 @@
 package org.rrhh.department.application.implementation;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.rrhh.department.application.usecase.DepartmentFindByCodeUseCase;
 import org.rrhh.department.domain.document.Department;
 import org.rrhh.department.domain.repository.DepartmentRepository;
@@ -15,7 +16,17 @@ public class DepartmentFindByCode implements DepartmentFindByCodeUseCase {
     }
 
     @Override
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     public Department getDepartmentByCode(String code) {
         return departmentRepository.findByCode(code);
+    }
+
+    public Department getDefaultDepartment(String code, Exception exception) {
+        return Department.builder()
+                .id(null)
+                .name("Default Department")
+                .description("Fake Department fallback")
+                .code("FD001")
+                .build();
     }
 }
