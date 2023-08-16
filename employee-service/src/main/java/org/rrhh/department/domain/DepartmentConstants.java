04642848a1d5dbc5d40ffa0faa6ef3696897bb84
Hector Cortez <updown.input@gmail.com>
2023-08-16 12:41:18 -0300

    Fix: uri changed for get department by code
    Signed-off-by: Hector Cortez <updown.input@gmail.com>

--- employee-service/src/main/java/org/rrhh/department/domain/DepartmentConstants.java
+++ employee-service/src/main/java/org/rrhh/department/domain/DepartmentConstants.java
@@ -4,6 +4,6 @@ public class DepartmentConstants {
 
     public static final String BASE_URL = "http://localhost:9091/api/v1/departments";
     public static final String MICROSERVICE_EUREKA_CLIENT_ID = "DEPARTMENT-SERVICE";
-    public static final String GET_BY_CODE_URI = "/api/v1/departments/code/{departmentCode}";
+    public static final String GET_BY_CODE_URI = "/api/v1/departments/{departmentCode}";
     public static final String PACKAGE = "org.rrhh.department.infrastructure.repository";
 }
