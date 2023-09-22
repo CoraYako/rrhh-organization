8f65792d9b608f12fc5255b6bd169dc012ff1e18
Héctor Cortez <updown.input@gmail.com>
2023-09-22 19:37:07 -0300

    Fix: dedicated endpoint to check properties updates
    Signed-off-by: Héctor Cortez <updown.input@gmail.com>

--- employee-service/src/main/java/org/rrhh/employee/infrastructure/controller/EmployeeMessageController.java
+++ employee-service/src/main/java/org/rrhh/employee/infrastructure/controller/EmployeeMessageController.java
@@ -1,10 +1,12 @@
 package org.rrhh.employee.infrastructure.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/employees")
 public class EmployeeMessageController {
