fd02ab31f58a2c941d8c9776faaf9684d228a196
Héctor Cortez <updown.input@gmail.com>
2023-09-22 00:05:50 -0300

    Feat: added more allowed url
    Signed-off-by: Héctor Cortez <updown.input@gmail.com>

--- organization-service/src/main/java/org/rrhh/organization/domain/OrganizationConstants.java
+++ organization-service/src/main/java/org/rrhh/organization/domain/OrganizationConstants.java
@@ -10,7 +10,11 @@ public class OrganizationConstants {
             "http://localhost:9191",
             "https://localhost:9191",
             "https://localhost",
-            "http://localhost"
+            "http://localhost",
+            "http://api-gateway:9191",
+            "http://api-gateway",
+            "http://172.17.0.2:9191",
+            "http://172.17.0.2"
     };
     public static final String[] ALLOWED_METHODS = {
             "GET",
